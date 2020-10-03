e83c9472c47b3e9a8b53b037061dfd0ec5c3a823
Andrey Lapitskiy <70910463+BBunny19@users.noreply.github.com>
2020-10-03 16:58:12 +0300

    fix: typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 
- function min (array) {
+exports.min = function min (array) {
   return Math.min.apply(null, array);
 }
 
- function max (array) {
+exports.max = function max (array) {
     return Math.max.apply(null, array);
 }
 
-function avg(array) {
+exports.avg = function avg(array) {
     var sum = 0;
     for(var i = 0; i < array.length; i++) {
         sum += array[i];
