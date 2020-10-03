50a9e813846b47a0e8a96ee78a97aebfaec21685
Andrey Lapitskiy <70910463+BBunny19@users.noreply.github.com>
2020-10-03 16:54:47 +0300

    init: fix t-a-p

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+ function min (array) {
+  return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+ function max (array) {
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+function avg(array) {
+    var sum = 0;
+    for(var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+ }
